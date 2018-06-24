244ce7a38dcf9632ed3931b24c296af6b9d4fc40
XinHui <handelxh@163.com>
2018-06-24 22:57:06 +0800

    modified for geth deploy

--- testjs/deploy.js
+++ testjs/deploy.js
@@ -4,7 +4,7 @@ if (typeof web3 !== 'undefined') {
   web3 = new Web3(web3.currentProvider);
 } else {
   // set the provider you want from Web3.providers
-  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 
 
@@ -17,11 +17,11 @@ if(!connected){
 }
 
 if(!web3.currentProvider)
-    web3.setProvider(new web3.providers.HttpProvider("http://localhost:7545"));
+    web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
 
 var accounts = web3.eth.accounts;
 web3.eth.defaultAccount = accounts[0];
-// console.log(accounts); 
+console.log(accounts); 
 
 // var balance = web3.eth.getBalance('0x160b41d953cad924c605371c52be9ea163837310');
 // console.log(balance); // instanceof BigNumber
@@ -31,14 +31,13 @@ const fs = require("fs");
 const solc = require('solc')
 
 let source = fs.readFileSync('../worldcup.sol', 'utf8');
-let compiledContract = solc.compile(source, 1);
+let compiledContract = solc.compile(source, 0x1);
+
 // console.log(compiledContract); 
 let abi = compiledContract.contracts[':WorldCup'].interface;
 let bytecode = compiledContract.contracts[':WorldCup'].bytecode;
-let gasEstimate = web3.eth.estimateGas({data: bytecode});
+let gasEstimate = web3.eth.estimateGas({data: '0x'+bytecode});
 let worldcup = web3.eth.contract(JSON.parse(abi));
-
-
 
 
 // console.log(worldcup); 
@@ -53,11 +52,11 @@ var myContractReturned = worldcup.new( {
 
        // e.g. check tx hash on the first call (transaction send)
        if(!myContract.address) {
-           console.log(myContract.transactionHash) // The hash of the transaction, which deploys the contract
+           console.log('1',myContract.transactionHash) // The hash of the transaction, which deploys the contract
        
        // check address on the second call (contract deployed)
        } else {
-           console.log(myContract.address) // the contract address
+           console.log('2',myContract.address) // the contract address
        }
 
        // Note that the returned "myContractReturned" === "myContract",
